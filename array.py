ba4ce3c77b99ac497310e8b32f355df0c8b8ccf5
lzx <bbbbbb_li@aliyun.com>
2020-10-23 17:08:01 +0800

    finish array.py and start linkedList.py

--- array.py
+++ array.py
@@ -16,7 +16,7 @@ class Array:
         self.check_index(index)
         return self._data[index]
 
-    def __setitem__(self, index: int, value: int):
+    def __setitem__(self, index: int, value: object):
         self.check_index(index)
         self._data[index] = value
 
@@ -26,6 +26,9 @@ class Array:
     def __iter__(self):
         for item in self._data:
             yield item
+
+    def __repr__(self):
+        return repr(self._data)
 
     def find(self, index: int) -> object:
         self.check_index(index)
@@ -40,7 +43,7 @@ class Array:
         except IndexError:
             return False
 
-    def insert(self, index: int, value: int) -> bool:
+    def insert(self, index: int, value: object) -> bool:
         try:
             self.check_index(index)
             if len(self) >= self._capacity:
@@ -49,10 +52,6 @@ class Array:
                 return self._data.insert(index, value)
         except IndexError:
             return False
-
-    def print_all(self):
-        for item in self:
-            print(item)
 
 
 def test_array():
@@ -66,7 +65,7 @@ def test_array():
     # assert array.insert(0, 100) is False
     # assert len(array) == 5
     # assert array.find(4) == 9
-    # assert array.delete(4) is True
+    # assert array.delete(4) is True 会调用函数，所以会执行
 
     array[3]
     array[3] = 1
@@ -76,10 +75,111 @@ def test_array():
     array._data.sort()
     array.print_all()
 
+# 合并两个有序数组。方法一：双指针，从前向后比较，往后挪动插入
 
-def sorted_array_merge(array1: Array, array2: Array) -> Array:
-    NotImplemented
+
+def sorted_array_merge(array1: Array, len1: int, array2: Array, len2: int) -> Array:
+    # 创建指针分别指向数组1和数组2
+    i, j = 0, 0
+    while(j < len2):
+        # 比较两个指针指向数的大小
+        if array2[j] <= array1[i]:
+            # 若数组2指针指向数小于等于数组1指针指向数
+            # 则数组1全部数后移一位，头部插入数组2指针指向数
+            for _ in range(len1-1, i-1, -1):
+                array1[_+1] = array1[_]
+            array1[i] = array2[j]
+            # 数组1，数组2指针后移一位，数组1长度增加1
+            j += 1
+            i += 1
+            len1 += 1
+        else:
+            # 若数组2指针指向数大于数组1指针指向数
+            # 如果数组1指针指向最后一个数据，则将数组2拼接在数组1后面
+            if i >= len1:
+                for _ in range(j, len2):
+                    array1[len1] = array2[_]
+                    len1 += 1
+                break
+            # 否则数组1指针后移一位
+            else:
+                i += 1
+    return array1
+
+# 合并两个有序数组。方法二：双指针，从前向后比较，第三方数组存储
+
+
+def sorted_array_merge2(array1: Array, len1: int, array2: Array, len2: int) -> Array:
+    array = Array(len1 + len2)
+    i, j, z = 0, 0, 0
+    while(i < len1 and j < len2):
+        if array1[i] < array2[j]:
+            array.insert(z, array1[i])
+            z += 1
+            i += 1
+        else:
+            array.insert(z, array2[j])
+            z += 1
+            j += 1
+    if i >= len1 and j < len2:
+        for _ in range(j, len2):
+            array.insert(z, array2[_])
+            z += 1
+    elif j >= len2 and i < len1:
+        for _ in range(i, len1):
+            array.insert(z, array1[_])
+            z += 1
+    return array
+
+# 合并两个有序数组。方法二：双指针，从后向前比较
+
+
+def sorted_array_merge3(array1: Array, len1: int, array2: Array, len2: int) -> Array:
+    len = len1 + len2
+    i, j, z = len1-1, len2-1, len-1
+    while(i >= 0 and j >= 0):
+        if array1[i] <= array2[j]:
+            array1[z] = array2[j]
+            j -= 1
+            z -= 1
+        else:
+            array1[z] = array1[i]
+            i -= 1
+            z -= 1
+    if (j >= 0):
+        for _ in range(0, j+1):
+            array1[_] = array2[_]
+    return array1
 
 
 if __name__ == "__main__":
-    test_array()
+    # test_array()
+
+    '''  
+    说明：
+        初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+        你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+    示例:
+        输入:
+            nums1 = [1,2,3,0,0,0], m = 3
+            nums2 = [2,5,6],       n = 3
+        输出: [1,2,2,3,5,6] 
+    '''
+
+    nums1, m = [2, 3, 3, 9, 10, 0, 0, 0], 5
+    nums2, n = [0, 1, 6], 3
+
+    array1 = Array(m+n)
+    array2 = Array(n)
+
+    for i in range(m+n):
+        array1.insert(i, nums1[i])
+
+    for i in range(n):
+        array2.insert(i, nums2[i])
+    print(array1, array2)
+
+    result1 = sorted_array_merge(array1, m, array2, n)
+    result2 = sorted_array_merge2(array1, m, array2, n)
+    result3 = sorted_array_merge3(array1, m, array2, n)
+    print(result1, result2, result3, sep='\n')
